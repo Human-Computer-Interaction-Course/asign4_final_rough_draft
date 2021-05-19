6a19aa088c924a3649fdbdfa2d51ac928e60b171
greseam <greseam@dunwoody.edu>
2021-05-19 17:09:18 -0500

    added more pages

---
+++ javascript/scripts.js
@@ -0,0 +1,12 @@
+/*
+    Author: Sean Gregor
+
+    The use of 2 or more user-defined functions
+    The use of the document.addEventListener() method
+    The use of an array
+    The use of decision making logic (if statements, switch statements)
+    Use of at least one document object method that targets element(s). Ex.  getElementsById(), getElementsByClass(), getElementsByTagName(), document.querySelector() etc...
+    Use of at least one document object property.  Ex. innerHTML, images, head, links, activeElement, etc...
+
+
+*/
